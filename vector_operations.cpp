c2a1c86d9ee08bca76ace64c2693ffb801c705d6
virogg <itisinter@gmail.com>
2024-02-23 15:09:06 +0300

    Added Vector operations

--- vector_operations.cpp
+++ vector_operations.cpp
@@ -1,1 +1,55 @@
 #include "vector_operations.h"
+
+//Overloading Point operators
+Point operator+(const Point& a, const Vector& b){
+    return {a.x + b.x, a.y + b.y};
+}
+Point operator-(const Point& a, const Vector& b){
+    return {a.x - b.x, a.y - b.y};
+}
+Point& Point::operator+=(const Vector& a){
+    x += a.x;
+    y += a.y;
+    return *this;
+}
+Point& Point::operator-=(const Vector& a){
+    x -= a.x;
+    y -= a.y;
+    return *this;
+}
+//Overloading Vector operators
+Vector operator+(const Vector& a, const Vector& b){
+    return {a.x + b.x, a.y + b.y};
+}
+Vector operator-(const Vector& a, const Vector& b){
+    return {a.x - b.x, a.y - b.y};
+}
+Vector operator*(const Vector& a, double c){
+    return {a.x * c, a.y * c};
+}
+Vector operator*(double c, Vector& a){
+    return a * c;
+}
+Vector operator/(const Vector& a, double c){
+    return {a.x / c, a.y / c};
+}
+Vector& Vector::operator+=(const Vector &a) {
+    x += a.x;
+    y += a.y;
+    return *this;
+}
+Vector& Vector::operator-=(const Vector &a) {
+    x -= a.x;
+    y -= a.y;
+    return *this;
+}
+Vector& Vector::operator*=(double c) {
+    x *= c;
+    y *= c;
+    return *this;
+}
+Vector& Vector::operator/=(double c) {
+    x /= c;
+    y /= c;
+    return *this;
+}
--- vector_operations.h
+++ vector_operations.h
@@ -1,4 +1,42 @@
 #ifndef GRAPHGHETTI_VECTOR_OPERATIONS_H
 #define GRAPHGHETTI_VECTOR_OPERATIONS_H
 
+#include "cmath"
+
+struct Vector{
+    double x;
+    double y;
+
+    double normalize(){
+        return std::sqrt(x*x + y*y);
+    }
+
+    Vector& operator+=(const Vector& a);
+    Vector& operator-=(const Vector& a);
+    Vector& operator*=(double c);
+    Vector& operator/=(double c);
+};
+struct Point{
+    double x;
+    double y;
+
+    Point& operator+=(const Vector& a);
+    Point& operator-=(const Vector& a);
+};
+// Операции линейного пространства
+Point operator+(const Point& a, const Vector& b);
+Point operator-(const Point& a, const Vector& b);
+// Операции над векторами
+Vector operator+(const Vector& a, const Vector& b);
+Vector operator-(const Vector& a, const Vector& b);
+Vector operator*(const Vector& a, double c);
+Vector operator*(double c, const Vector& a);
+Vector operator/(const Vector& a, double c);
+
+
+
+
+
+
+
 #endif //GRAPHGHETTI_VECTOR_OPERATIONS_H
