43b734034da945b20eac0a4d8b9149bb8cef67e3
virogg <itisinter@gmail.com>
2024-03-03 17:28:20 +0300

    Added Graph class and implemented its functions

---
+++ graph.cpp
@@ -0,0 +1,22 @@
+#include "graph.h"
+#include <iostream>
+
+Graph::Graph() {
+    size_t V, E;
+    std::cin >> V >> E;
+    graph_.resize(V);
+    for (int i = 0; i < E; ++i) {
+        int u, v;
+        std::cin >> u >> v;
+        graph_[u].push_back(v);
+        graph_[v].push_back(u);
+    }
+}
+
+const std::vector<int>& Graph::get_neighbours(size_t vertex_num) const {
+    return graph_[vertex_num];
+}
+
+size_t Graph::size() const {
+    return graph_.size();
+}
---
+++ graph.h
@@ -0,0 +1,19 @@
+#ifndef GRAPHGHETTI_GRAPH_H
+#define GRAPHGHETTI_GRAPH_H
+
+#include <vector>
+#include "vector_operations.h"
+
+
+class Graph {
+public:
+    Graph();
+
+    [[nodiscard]] size_t size() const;
+
+    [[nodiscard]] const std::vector<int>& get_neighbours(size_t vertex_num) const;
+private:
+    std::vector<std::vector<int>> graph_;
+};
+
+#endif //GRAPHGHETTI_GRAPH_H
