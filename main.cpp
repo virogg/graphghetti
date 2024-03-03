4158c83cba5d367fa4e333b0b1a43dead1c36719
virogg <itisinter@gmail.com>
2024-03-03 17:29:37 +0300

    Changed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
-#include "iostream"
+#include <iostream>
+#include "fruchterman_reingold.h"
+#include "graph.h"
+#include "bmp_processor.h"
 
-int main(){
+int main() {
+    Graph graph;
 
+    int width = 1000, height = 1000;
 
+    BMPProcessor bmpProcessor(width, height);
 
+    ForceDirectedPlacement fdp(graph, width, height);
+
+    fdp.run_algorithm(1000);
+
+    bmpProcessor.drawGraph(fdp.get_vertices(), graph);
+    bmpProcessor.saveImage("graph.bmp");
 
     return 0;
 }
